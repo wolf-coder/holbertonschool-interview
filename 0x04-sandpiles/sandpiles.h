b7983ff60e96dd734976b4ec39f476783d754151
wolf-coder <cuore.fakhri@gmail.com>
2022-01-16 15:12:30 +0100

    fix

--- 0x04-sandpiles/sandpiles.h
+++ 0x04-sandpiles/sandpiles.h
@@ -2,9 +2,9 @@
 #define SANDPILES
 
 #include <stdio.h>
-void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
-
+#include <stdlib.h>
 void Target_cases(int target_case[3][3], int grid1[3][3]);
 int to_topple_more(int target_case[3][3], int *tople);
 static void print_grid(int grid[3][3]);
+void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
 #endif
