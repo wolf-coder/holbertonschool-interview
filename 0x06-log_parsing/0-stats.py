a075d863946c4d5dd0e5149c0ef1a4d83de18c41
Bakouti Fakhri <cuore.fakhri@gmail.com>
2022-07-07 20:34:42 +0100

    first try

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -5,7 +5,7 @@ import sys
 Files10_Size = 0
 counter = 0
 
-dict_codes_counter = {'200': 0, '301': 0, '400': 0, '401': 0,
+Dicts_codes = {'200': 0, '301': 0, '400': 0, '401': 0,
                       '403': 0, '404': 0, '405': 0, '500': 0}
 
 try:
@@ -14,14 +14,14 @@ try:
         if len(line_list) > 2:
             code = line_list[-2]
             size = int(line_list[-1])
-            if code in dict_codes_counter.keys():
-                dict_codes_counter[code] += 1
+            if code in Dicts_codes.keys():
+                Dicts_codes[code] += 1
             Files10_Size += size
             counter += 1
 
         if counter == 10:
             print("File size: {:d}".format(Files10_Size))
-            for k, v in sorted(dict_codes_counter.items()):
+            for k, v in sorted(Dicts_codes.items()):
                 if v != 0:
                     print("{}: {:d}".format(k, v))
             counter = 0
@@ -31,6 +31,6 @@ except Exception:
 
 finally:
     print("File size: {}".format(Files10_Size))
-    for k, v in sorted(dict_codes_counter.items()):
+    for k, v in sorted(Dicts_codes.items()):
         if v != 0:
             print("{}: {}".format(k, v))
