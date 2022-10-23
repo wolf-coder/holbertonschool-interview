9019e3af11c125b035ba47f5cd19ca5c7f08aa8c
wolf-coder <cuore.fakhri@gmail.com>
2022-10-23 02:34:32 +0100

    Final

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,23 +1,21 @@
 #!/usr/bin/node
-
 const request = require('request');
-const M_ID = process.argv[2];
-const url = 'https://swapi-api.hbtn.io/api/films/' + M_ID;
-const Dict = {};
+const Id = process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/' + Id;
 
-request(url, function (err, res, body) {
-  if (err) console.log(err);
-  const characters = JSON.parse(body).characters;
-  characters.forEach((char) => {
-    request(char, function (err, res, body) {
-      if (err) console.log(err);
-      const name = JSON.parse(body).name;
-      Dict[char] = name;
-      if (Object.values(Dict).length === characters.length) {
-        characters.forEach((character) => {
-          console.log(Dict[character]);
-        });
-      }
+request(url, async (err, response, body) => {
+  if (err) {
+    console.log(err);
+  }
+  for (const charId of JSON.parse(body).characters) {
+    await new Promise((resolve, reject) => {
+      request(charId, (err, response, body) => {
+        if (err) {
+          reject(err);
+        }
+        console.log(JSON.parse(body).name);
+        resolve();
+      });
     });
-  });
+  }
 });
