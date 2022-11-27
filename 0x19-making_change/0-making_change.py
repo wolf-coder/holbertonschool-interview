ca774a628da4cdf08d6d5ac921714f6d4c306da4
wolf-coder <cuore.fakhri@gmail.com>
2022-11-27 23:34:41 +0100

    Fix

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -6,20 +6,20 @@ Auxiliary space: O(total_amount_required) for using extra matrix space
 import sys
 def makeChange(coins, total):
     """
-    + coins: List containing the possible coins to use.
-    + total: 
-    + Return: fewest number of coins needed to meet total
-        If total is 0 or less, return 0
-        If total cannot be met by any number of coins you have, return -1
+    determine the fewest number of coins needed
+    to meet a given amount total
     """
+    number_coins = 0
+    cents = 0
     if total <= 0:
         return 0
 
-    coins.sort(reverse=True)
-    count = 0
+    coins = sorted(coins, reverse=True)
+
     for coin in coins:
-        count += total // coin
-        total = total % coin
-    if total:
-        return -1
-    return count
+        while cents + coin <= total:
+            cents += coin
+            number_coins += 1
+        if cents == total:
+            return number_coins
+    return -1
