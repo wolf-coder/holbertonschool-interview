6917aabbbbf3373d72000984b2f3af154a1133be
wolf-coder <cuore.fakhri@gmail.com>
2022-11-27 23:37:46 +0100

    Fix 0

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -3,23 +3,23 @@
 Time complexity: O(Array_size x total_amount_required).
 Auxiliary space: O(total_amount_required) for using extra matrix space
 """
-import sys
 def makeChange(coins, total):
+    """Determines the fewest number of coins needed to meet a given
+    amount total when given a pile of coins of different values.
     """
-    determine the fewest number of coins needed
-    to meet a given amount total
-    """
-    number_coins = 0
-    cents = 0
     if total <= 0:
         return 0
-
-    coins = sorted(coins, reverse=True)
-
-    for coin in coins:
-        while cents + coin <= total:
-            cents += coin
-            number_coins += 1
-        if cents == total:
-            return number_coins
-    return -1
+    rem = total
+    coins_count = 0
+    coin_idx = 0
+    sorted_coins = sorted(coins, reverse=True)
+    n = len(coins)
+    while rem > 0:
+        if coin_idx >= n:
+            return -1
+        if rem - sorted_coins[coin_idx] >= 0:
+            rem -= sorted_coins[coin_idx]
+            coins_count += 1
+        else:
+            coin_idx += 1
+    return coins_count
