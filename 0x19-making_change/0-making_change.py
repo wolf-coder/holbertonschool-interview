97a2ecc61d4248841f67cf41c2d336847cbc6ccf
wolf-coder <cuore.fakhri@gmail.com>
2022-11-27 23:50:57 +0100

    F2

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -12,9 +12,10 @@ def makeChange(coins, total):
         If total is 0 or less, return 0
         If total cannot be met by any number of coins you have, return -1
     """
+    if total<=0:
+        return 0
 
     matrix = [0 for i in range(total + 1)]
-
 
     matrix[0] = 0
 
@@ -23,8 +24,6 @@ def makeChange(coins, total):
         matrix[i] = sys.maxsize # (Initialise the matrix elements to Infinity)
 
     for i in range(1, total + 1):
-
-
         for j in range(len(coins)):
             if (coins[j] <= i):
                 sub_res = matrix[i - coins[j]]
