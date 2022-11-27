29d5fa4482a82822eae6c43e004c3a242839409f
wolf-coder <cuore.fakhri@gmail.com>
2022-11-27 23:32:45 +0100

    Fix

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -12,27 +12,14 @@ def makeChange(coins, total):
         If total is 0 or less, return 0
         If total cannot be met by any number of coins you have, return -1
     """
-    coins = sorted(coins, reverse=True)
     if total <= 0:
         return 0
 
-    matrix = [0 for i in range(total + 1)]
-
-    matrix[0] = 0
- 
-
-    for i in range(1, total + 1):
-        matrix[i] = sys.maxsize # (Initialise the matrix elements to Infinity)
-
-    for i in range(1, total + 1):
-        for j in range(len(coins)):
-            if (coins[j] <= i):
-                sub_res = matrix[i - coins[j]]
-                if (sub_res != sys.maxsize and
-                    sub_res + 1 < matrix[i]):
-                    matrix[i] = sub_res + 1
-
-    if matrix[total] == sys.maxsize:
+    coins.sort(reverse=True)
+    count = 0
+    for coin in coins:
+        count += total // coin
+        total = total % coin
+    if total:
         return -1
-
-    return matrix[total]
+    return count
