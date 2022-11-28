d91252273086130b3965869b1bae3c23531675b9
wolf-coder <cuore.fakhri@gmail.com>
2022-11-28 17:45:53 +0100

    check Fail

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -3,15 +3,10 @@
 Time complexity: O(Array_size x total_amount_required).
 Auxiliary space: O(total_amount_required) for using extra matrix space
 """
-# INF = 100000000Checker 13,14 Falls 
-# INF = 10000000 Checker 13,14 Falls
-# INF = 5500000  Cheker  13    Falls
-# INF = 1000000  Cheker  13    Falls
-# INF = 100000   Cheker  13,5  Falls
-# INF = 90000    Cheker  13,5  Falls
-INF = 50000    #Cheker  13,5  Falls
-# INF = 10000    Cheker  5     Falls
-# INF = 1000     Cheker  5     Falls
+
+INF = 1000000  # Avoiding `sys.maxsize` for runtime evaluation
+
+
 def makeChange(coins, total):
     """
     + coins: List containing the possible coins to use.
@@ -27,20 +22,17 @@ def makeChange(coins, total):
     matrix = [0 for i in range(total + 1)]
 
     matrix[0] = 0
- 
 
     for i in range(1, total + 1):
-        matrix[i] = INF # (Initialise the matrix elements to Infinity)
+        matrix[i] = INF  # (Initialise the matrix elements to Infinity)
 
     for i in range(1, total + 1):
         for j in range(len(coins)):
             if (coins[j] <= i):
                 sub_res = matrix[i - coins[j]]
-                if (sub_res != INF and
-                    sub_res + 1 < matrix[i]):
+                if (sub_res != INF and sub_res + 1 < matrix[i]):
                     matrix[i] = sub_res + 1
 
     if matrix[total] == INF:
         return -1
-
     return matrix[total]
