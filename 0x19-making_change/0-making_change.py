7ff38cf972f31a6f54371941c0067728756b2a2e
wolf-coder <cuore.fakhri@gmail.com>
2022-11-28 15:19:46 +0100

    A

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -3,8 +3,7 @@
 Time complexity: O(Array_size x total_amount_required).
 Auxiliary space: O(total_amount_required) for using extra matrix space
 """
-
-INF = 10000
+import sys
 def makeChange(coins, total):
     """
     + coins: List containing the possible coins to use.
@@ -13,26 +12,27 @@ def makeChange(coins, total):
         If total is 0 or less, return 0
         If total cannot be met by any number of coins you have, return -1
     """
-    k = len(coins)
-    n = total
-    d= coins
+    coins = sorted(coins, reverse=True)
     if total <= 0:
         return 0
-    M = [0]*(n+1)
-    S = [0]*(n+1)
-#    breakpoint()
-    for j in range(1, n+1):
-        minimum = INF
-        coin = 0
 
-        for i in range(1, k):
-            #breakpoint()
-            if(j >= d[i]):
-                minimum = min(minimum, 1+M[j-d[i]])
-                coin = i
-        M[j] = minimum
-        S[j] = coin
-    if M[n] == INF:
+    matrix = [0 for i in range(total + 1)]
+
+    matrix[0] = 0
+ 
+
+    for i in range(1, total + 1):
+        matrix[i] = sys.maxsize # (Initialise the matrix elements to Infinity)
+
+    for i in range(1, total + 1):
+        for j in range(len(coins)):
+            if (coins[j] <= i):
+                sub_res = matrix[i - coins[j]]
+                if (sub_res != sys.maxsize and
+                    sub_res + 1 < matrix[i]):
+                    matrix[i] = sub_res + 1
+
+    if matrix[total] == sys.maxsize:
         return -1
-    return M[n]
 
+    return matrix[total]
