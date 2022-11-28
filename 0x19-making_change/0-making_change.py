683106d4a1c1b2ec4e79b072437007602a456249
wolf-coder <cuore.fakhri@gmail.com>
2022-11-28 18:07:18 +0100

    7000

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -6,7 +6,8 @@ Auxiliary space: O(total_amount_required) for using extra matrix space
 
 #INF = 1000000  # Avoiding `sys.maxsize` for runtime evaluation 13,14
 #INF = 100000  # Avoiding `sys.maxsize` for runtime evaluation
-INF = 50000 # 13,14
+#INF = 50000 # 5
+INF = 70000 # 5
 def makeChange(coins, total):
     """
     + coins: List containing the possible coins to use.
