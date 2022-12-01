75cb263530cb5eb4e0d43a074bd29f9eeb25cc09
wolf-coder <cuore.fakhri@gmail.com>
2022-12-01 23:45:30 +0100

    Fix betty

--- 0x1E-wild_cmp/0-wildcmp.c
+++ 0x1E-wild_cmp/0-wildcmp.c
@@ -4,19 +4,19 @@
 /**
  * wildcmp - compares two strings and returns 1 if the strings can be
  * considered identical, otherwise return 0.
-
- * @s2: string to compare that can have wildcards
+ * @s1: string to compare.
+ * @s2: string to compare (can have wildcards: `*`).
  * Return: 1 if strings are identical, else 0
  */
 int wildcmp(char *s1, char *s2)
 {
 	if (!*s1)
 	{
-		if (*s2 == '*')
+		if (*s2 == '*') /*wildcard: Skip recursively*/
 			return (wildcmp(s1, s2 + 1));
 		return (!*s2);
 	}
-	if (*s2 == '*')
+	if (*s2 == '*') /*wildcard: Skip recursively*/
 		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
 	if (*s1 == *s2)
 		return (wildcmp(s1 + 1, s2 + 1));
