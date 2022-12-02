d16fab8e9382dea7e70739d6d8b2addf315e594a
wolf-coder <cuore.fakhri@gmail.com>
2022-12-02 23:04:55 +0100

    Check

---
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""
+Calculating the perimeter of the island described in grid
+"""
+import numpy as np
+
+
+def island_perimeter(grid):
+    """
+    Function that returns the perimeter of the island described in grid:
+      + grid is a list of list of integers:
+      + 0 represents water
+      + 1 represents land
+      + Each cell is square, with a side length of 1
+      + Cells are connected horizontally/vertically (not diagonally).
+      + grid is rectangular, with its width and height not exceeding 100
+      + The grid is completely surrounded by water
+      + There is only one island (or nothing).
+      + The island doesn’t have “lakes” (water inside that isn’t connected to the water surrounding the island).
+    """
+    def Two_cell(i, j):
+        """
+        Function that calculate the side to substract
+        i,j: cell coordinates
+        """
+        subs = 0
+        # check right
+        if j < C -1 and grid[i][j+1]==1:
+            subs += 2
+        # check right
+        if i < R -1 and grid[i+1][j]==1:
+            subs += 2
+        return -subs
+
+    R = len(grid)  # numer of rows
+    C = len(grid[0])  # numer of columns
+    count = 0
+    for i in range(R):
+        for j in range(C):
+            if grid[i][j] == 1:
+                count += (4 + Two_cell(i, j))
+                
+    return count
